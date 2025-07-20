5e954f3ab216c00c100c7559ed2808685ebfbea8
Aaron Santos <2302042@utrivieramaya.edu.mx>
2025-07-19 23:42:50 -0500

    feat: vistas de los modelos

---
+++ resources/views/roles/index.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Gestión de Roles') }}
+        </h2>
+    </x-slot>
+
+    <livewire:roles-manager />
+</x-app-layout>
---
+++ resources/views/user-notes/index.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Gestión de Notas') }}
+        </h2>
+    </x-slot>
+
+    <livewire:user-notes-manager />
+</x-app-layout>
